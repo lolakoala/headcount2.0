87e1c162c1daaaa71642923af716aeb9e89f8106
Lola Brenner <amandadbrenner@gmail.com>
2017-10-03 08:03:13 -0600

    reduces data array and assigns to this.data in DisctrictRepository constructor

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,13 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = data.reduce((acc, obj) => {
+  if (acc[obj.Location]) {
+    acc[obj.Location].push({ 'TimeFrame': obj.TimeFrame, 'Data': obj.Data });
+  } else {
+    acc[obj.Location] = [];
+  }
+  return acc;
+}, {})
+  }
 
 }
