a77637c0147228aefb9662a6fe3cc4ca3e7d68b3
Lola Brenner <amandadbrenner@gmail.com>
2017-10-03 12:20:29 -0600

    changes function for this.data to format the data into an array of objects with the keys specified in iteration 1 test

--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,32 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.data = data.reduce((acc, obj) => {
-  if (acc[obj.Location]) {
-    acc[obj.Location].push({ 'TimeFrame': obj.TimeFrame, 'Data': obj.Data });
-  } else {
-    acc[obj.Location] = [];
+    this.data = this.cleanData(data);
   }
-  return acc;
-}, {})
+
+  cleanData(data) {
+    let locationObjectArray = [];
+    let locationArray = data.reduce((acc, obj) => {
+                          if (!acc.find((location) => {return location === obj.Location})) {
+                            acc.push(obj.Location);
+                          }
+                          return acc;
+                        }, []);
+
+    locationArray.forEach((district) => {
+      locationObjectArray.push({ 'location': district, 'data': { } });
+    });
+
+    let finalArray = data.reduce((acc, obj) => {
+      let objectWeNeed = acc.find((elem) => {return obj.Location === elem.location});
+      objectWeNeed.data[obj.TimeFrame] = obj.Data;
+      return acc;
+    }, locationObjectArray)
+
+    return finalArray;
+  }
+
+  findByName(string) {
+
   }
 
 }
