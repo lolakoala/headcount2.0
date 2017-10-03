b9cb61656fda66f6e5824eed5a756e12e4f98fed
Lola Brenner <amandadbrenner@gmail.com>
2017-10-03 12:29:24 -0600

    adds comments to explain function

--- src/helper.js
+++ src/helper.js
@@ -4,24 +4,28 @@ export default class DistrictRepository {
   }
 
   cleanData(data) {
+    // make an empty array to push objects into
     let locationObjectArray = [];
+    // reduce data comming in to an array of location strings
     let locationArray = data.reduce((acc, obj) => {
                           if (!acc.find((location) => {return location === obj.Location})) {
                             acc.push(obj.Location);
                           }
                           return acc;
                         }, []);
-
+    // iterate through array of location strings to push an object skeleton into locationObjectArray for each location
     locationArray.forEach((district) => {
       locationObjectArray.push({ 'location': district, 'data': { } });
     });
-
+    // finally, use locationObjectArray which now has object skeletons as an accumulator
+    // to reduce data coming in by finding the object in the locationObjectArray with matching location
+    // and adding a year key with value of data to corresponding object for each object in data
     let finalArray = data.reduce((acc, obj) => {
       let objectWeNeed = acc.find((elem) => {return obj.Location === elem.location});
       objectWeNeed.data[obj.TimeFrame] = obj.Data;
       return acc;
     }, locationObjectArray)
-
+    // one last thing- gimme the array that we formatted so nicely to be what we want
     return finalArray;
   }
 
