12a459a5a5700df0ea68f0373997aabd1d749341
Jason Hughes <jasonhughes088@gmail.com>
2017-10-05 16:56:14 -0600

    search test working

---
+++ src/Components/Search/Search.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Search from './Search.js';
+import { shallow } from 'enzyme';
+
+describe('Search', () => {
+  let wrapper = shallow(<Search />);
+
+  it('should have a search container', () => {
+    let searchContainer = wrapper.find('.searchContainer');
+
+    expect(searchContainer.length).toEqual(1);
+  });
+
+  it('should have an input', () => {
+    let input = wrapper.find('input');
+
+    expect(input.length).toEqual(1);
+  });
+})
