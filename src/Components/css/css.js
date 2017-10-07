4d3a9acc4ff353851fdc0b7a0ff9055e5c8746ce
Jason Hughes <jasonhughes088@gmail.com>
2017-10-07 11:32:38 -0600

    fix some css

--- src/Components/css/css.js
+++ src/Components/css/css.js
@@ -1,4 +1,5 @@
 import App from '../App/App.css';
 import Card from '../Card/Card.css';
 import CardContainer from '../CardContainer/CardContainer.css';
-import Search from '../Search/Search.css'
+import Search from '../Search/Search.css';
+import ComparisonCard from '../ComparisonCard/ComparisonCard.css';
